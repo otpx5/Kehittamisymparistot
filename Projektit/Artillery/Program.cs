b015ddee5b36d9af74e65cbed5930f353f16fe76
otpx5a <otpx5a@gmail.com>
2025-04-22 14:08:25 +0300

    Ground + Artillary

--- Projektit/Artillery/Program.cs
+++ Projektit/Artillery/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Artillery
+﻿
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Raylib_cs;
+
+namespace Artillery
 {
     internal class Program
     {
+        Rectangle Ground;
+        Rectangle Player;
+        Vector2 GunDirection;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program game = new Program();
+            game.init();
+
+        }
+        private void init()
+        {
+            Raylib.InitWindow(600, 600, "Artillery");
+            Ground = new Rectangle(0, Raylib.GetScreenHeight() - 50, Raylib.GetScreenWidth(), 50);
+            Player = new Rectangle(50, Ground.Y-10,10, 10);
+            GunDirection = new Vector2(0, -1 );
+            while (Raylib.WindowShouldClose() == false)
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.SkyBlue);
+                Raylib.DrawRectangleRec(Ground,Color.Green);
+                Raylib.DrawRectangleRec(Player,Color.Red);
+                Vector2 GunStart = new Vector2(Player.X + Player.Width / 2, Player.Y);
+                Raylib.DrawLineV(GunStart,GunStart + GunDirection * 10, Color.Black);
+                Raylib.EndDrawing();
+            }
         }
     }
 }
